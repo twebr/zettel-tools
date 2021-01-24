0ddb1cbc0b621718b6e1eea2dd77926b1d19cbd8
Thijs Meeuwisse <thijs@meeuwis.se>
2021-01-24 18:34:34 +0100

    Clean up code for check_integrity

--- check_integrity.py
+++ check_integrity.py
@@ -5,11 +5,11 @@ Loosely based on https://github.com/crelder/zettelkasten
 import argparse
 import re
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Generator
 from collections import defaultdict
 
 
-def list_duplicates(seq):
+def list_duplicates(seq: List) -> Generator[Tuple, None, None]:
     """Helper function to list duplicates.
 
     See https://stackoverflow.com/a/5419576
@@ -23,7 +23,7 @@ def list_duplicates(seq):
 
 
 def get_note_paths(directory: str, extensions: List[str]) -> List[Path]:
-    """Returns a list of all paths to notes in directory tree at given path.
+    """Returns a list of all paths to notes in the given directory.
     Filters out results based on extension."""
 
     patterns = ['*.{}'.format(ext) for ext in extensions]
@@ -42,10 +42,20 @@ def get_id_from_path(path: Path, separator: str = ' ') -> str:
     return path.stem.split(separator)[0]
 
 
+def get_ids_with_incorrect_format(note_paths: List[Path], id_pattern: str) -> List[Tuple[str, Path]]:
+    """Check if IDs have the correct format"""
+
+    zettel_ids = [get_id_from_path(path) for path in note_paths]
+
+    return [(zettel_id, note_paths[loc])
+            for loc, zettel_id in enumerate(zettel_ids)
+            if not re.match(id_pattern, zettel_id)]
+
+
 def get_duplicate_ids(note_paths: List[Path], id_pattern: str) -> List[Tuple[str, List[Path]]]:
     """Checks for duplicate IDs
 
-    Thi smethod assumes the IDs are in the correct format.
+    This method assumes the IDs are in the correct format.
 
     :return List of 2-tuples of the form (id, List[Path]) for each ID that
     occurs more than once
@@ -59,7 +69,7 @@ def get_duplicate_ids(note_paths: List[Path], id_pattern: str) -> List[Tuple[str
     return duplicates
 
 
-def get_broken_links(note_paths: List[Path], id_pattern: str):
+def get_broken_links(note_paths: List[Path], id_pattern: str) -> List[Tuple[str, Path]]:
     """Checks for broken links (links that do not point to an existing ID)
 
     Checks for the wikilink styles [[ZETTEL_ID]] and [[ZETTEL_ID Optional text]]
@@ -69,16 +79,6 @@ def get_broken_links(note_paths: List[Path], id_pattern: str):
     pattern = re.compile(r"\[{2}" + id_pattern + r"[^]\n]*]{2}")
     broken_links = []
 
-    # # OPTION 1
-    # for path in note_paths:
-    #     with path.open(mode='r') as file:
-    #         for line in file:
-    #             for match in pattern.finditer(line):
-    #                 print('Found {}'.format(match.group(1)))
-    #                 if match.group(1) not in ids:
-    #                     broken_links.append((match.group(1), path))
-
-    # OPTION 2
     for path in note_paths:
         for match in pattern.finditer(path.read_text()):
             # print('Found {}'.format(match.group(1)))
@@ -86,16 +86,6 @@ def get_broken_links(note_paths: List[Path], id_pattern: str):
                 broken_links.append((match.group(1), path))
 
     return broken_links
-
-
-def get_ids_with_incorrect_format(note_paths: List[Path], id_pattern: str) -> List[Tuple[str, Path]]:
-    """Check if IDs have the correct format"""
-
-    zettel_ids = [get_id_from_path(path) for path in note_paths]
-
-    return [(zettel_id, note_paths[loc])
-            for loc, zettel_id in enumerate(zettel_ids)
-            if not re.match(id_pattern, zettel_id)]
 
 
 def main():
@@ -124,7 +114,7 @@ def main():
     # Check for duplicate zettel IDs
     duplicate_ids = get_duplicate_ids(note_paths, args.id_pattern)
     if duplicate_ids:
-        print("The following IDs exist several times:")
+        print("The following IDs are not unique:")
         for zettel_id, paths in duplicate_ids:
             print("- {}, for the files: ".format(zettel_id))
             for path in paths:
